484b1c439482e431b3b4ff8638ebcffc27f21bbc
LG-blip <g021306@gmail.com>
2024-03-02 20:56:14 +0530

    Updated files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) { slideIndex = 1 }
+//   if (n < 1) { slideIndex = slides.length }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
+
+var images = ["./assets/chickmanglur-scenery.jpg", "./assets/chickmanglur-scene.jpg", "./assets/rectangle-27.png"]
+
+var imageChange = document.getElementById("box")
+
+var i = 0;
+setInterval(function() {
+      imageChange.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+}, 3000);
